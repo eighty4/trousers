1a6c5f6941437e668b36abc440ccec2e69019e0f
Adam McKee <adam.be.g84d@gmail.com>
2022-11-09 22:18:51 -0600

    refactor config builder api

--- packages/trousers-api/src/TrouserConfig.ts
+++ packages/trousers-api/src/TrouserConfig.ts
@@ -1,9 +1,6 @@
 import type {BankDataStore, TransactionDataStore} from 'trousers-data-interfaces'
 
 import type {LinkConfig, PlaidClientConfig} from './PlaidClient'
-import {configureDataRoutes} from './rest/configureDataRoutes'
-import {configureLinkRoutes} from './rest/configureLinkRoutes'
-import {ExpressRestProvider} from './rest/ExpressRestProvider'
 import type {TrouserRestConfig} from './rest/TrouserRestConfig'
 import type {TrouserRestProvider} from './rest/TrouserRestProvider'
 
@@ -14,34 +11,4 @@ export interface TrouserConfig {
     restConfig: TrouserRestConfig
     restProvider: TrouserRestProvider
     transactionDataStore: TransactionDataStore
-}
-
-export const populateDefaultConfig = (cfg: Partial<TrouserConfig>) => {
-    if (!cfg.restProvider) {
-        cfg.restProvider = new ExpressRestProvider()
-    }
-    if (!cfg.bankDataStore) {
-        throw new Error('no bank data store')
-    }
-    if (!cfg.transactionDataStore) {
-        throw new Error('no transaction data store')
-    }
-    if (!cfg.restConfig) {
-        cfg.restConfig = {
-            routeConfigurers: [],
-            port: 8000,
-        }
-    }
-    if (!cfg.restConfig.routeConfigurers.length) {
-        cfg.restConfig.routeConfigurers.push(
-            configureDataRoutes,
-            configureLinkRoutes,
-        )
-    }
-    if (!cfg.linkConfig) {
-        cfg.linkConfig = {
-            appName: 'a Trouser app',
-            webhookUrlPrefix: 'http://localhost:3001',
-        }
-    }
 }
--- packages/trousers-api/src/Trousers.ts
+++ packages/trousers-api/src/Trousers.ts
@@ -1,36 +1,117 @@
 import type {BankDataStore, TransactionDataStore} from 'trousers-data-interfaces'
 
-import type {PlaidClientConfig} from './PlaidClient'
+import type {LinkConfig, PlaidClientConfig} from './PlaidClient'
 import type {TrouserConfig} from './TrouserConfig'
 import type {TrouserRestProvider} from './rest/TrouserRestProvider'
+import {configureDataRoutes} from './rest/configureDataRoutes'
+import {configureLinkRoutes} from './rest/configureLinkRoutes'
+
+class TrousersBuilder {
+    plaidClientConfig: PlaidClientConfig
+    linkConfig: Partial<LinkConfig> = {}
+    bankDataStore?: BankDataStore
+    transactionDataStore?: TransactionDataStore
+    restProvider?: TrouserRestProvider
+
+    constructor(plaidClientConfig: PlaidClientConfig) {
+        this.plaidClientConfig = plaidClientConfig
+    }
+
+    createConfig(): TrouserConfig {
+        if (!this.restProvider) {
+            throw new Error('TrouserRestProvider was not configured with Trousers.restProvider')
+        }
+        if (!this.bankDataStore) {
+            throw new Error('BankDataStore was not configured with Trousers.dataStore or Trousers.bankDataStore')
+        }
+        if (!this.transactionDataStore) {
+            throw new Error('TransactionDataStore was not configured with Trousers.dataStore or Trousers.transactionDataStore')
+        }
+        if (!this.linkConfig.appName) {
+            this.linkConfig.appName = 'a Trousers app'
+        }
+        if (!this.linkConfig.webhookUrlPrefix) {
+            this.linkConfig.webhookUrlPrefix = 'http://localhost:3001'
+        }
+        return {
+            plaidClientConfig: this.plaidClientConfig,
+            restProvider: this.restProvider,
+            bankDataStore: this.bankDataStore,
+            transactionDataStore: this.transactionDataStore,
+            restConfig: {
+                port: 8000,
+                routeConfigurers: [
+                    configureDataRoutes,
+                    configureLinkRoutes,
+                ],
+            },
+            linkConfig: this.linkConfig as LinkConfig,
+        }
+    }
+}
 
 class Trousers {
 
-    static withClientConfig(cfg: PlaidClientConfig): Trousers {
-        return new Trousers(cfg)
+    static withPlaidCredentials(clientId: string, secret: string): Trousers {
+        if (!clientId || !secret) {
+            throw new Error('Trousers.withPlaidCredentials arguments are null or undefined')
+        }
+        return new Trousers({clientId, secret})
     }
 
-    private cfg: Partial<TrouserConfig> = {}
+    private builder: TrousersBuilder
 
-    private constructor(plaidClientConfig: PlaidClientConfig) {
-        this.cfg.plaidClientConfig = plaidClientConfig
+    constructor(plaidClientConfig: PlaidClientConfig) {
+        this.builder = new TrousersBuilder(plaidClientConfig)
+    }
+
+    linkConfig(linkConfig: LinkConfig): Trousers {
+        if (!linkConfig) {
+            throw new Error('Trousers.linkConfig is null or undefined')
+        }
+        this.builder.linkConfig = {
+            ...this.builder.linkConfig,
+            ...linkConfig,
+        }
+        return this
+    }
+
+    dataStore(dataStore: BankDataStore & TransactionDataStore): this {
+        if (!dataStore) {
+            throw new Error('Trousers.dataStore is null or undefined')
+        }
+        this.builder.bankDataStore = dataStore
+        this.builder.transactionDataStore = dataStore
+        return this
     }
 
     bankDataStore(bankDataStore: BankDataStore): this {
-        this.cfg.bankDataStore = bankDataStore
+        if (!bankDataStore) {
+            throw new Error('Trousers.bankDataStore is null or undefined')
+        }
+        this.builder.bankDataStore = bankDataStore
         return this
     }
 
     transactionDataStore(transactionDataStore: TransactionDataStore): this {
-        this.cfg.transactionDataStore = transactionDataStore
+        if (!transactionDataStore) {
+            throw new Error('Trousers.transactionDataStore is null or undefined')
+        }
+        this.builder.transactionDataStore = transactionDataStore
         return this
     }
 
     restProvider(restProvider: TrouserRestProvider): this {
-        this.cfg.restProvider = restProvider
+        if (!restProvider) {
+            throw new Error('Trousers.restProvider is null or undefined')
+        }
+        this.builder.restProvider = restProvider
         return this
     }
 
+    start(): void {
+
+    }
 }
 
 export default Trousers
