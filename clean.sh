b35edaeb6573bdb08edde13f8fe341d6f93259f8
Adam McKee <adam.be.g84d@gmail.com>
2022-11-17 00:55:53 -0600

    clean build output

---
+++ clean.sh
@@ -0,0 +1,14 @@
+
+rm -f example-fintech-startup/*.log
+rm -f example-fintech-startup/tsconfig.tsbuildinfo
+rm -f example-fintech-startup/src/*.js
+rm -f example-fintech-startup/src/*.d.ts
+rm -f example-fintech-startup/src/**/*.js
+rm -f example-fintech-startup/src/**/*.d.ts
+
+rm -f packages/*/*.log
+rm -f packages/*/tsconfig.tsbuildinfo
+rm -f packages/*/src/*.js
+rm -f packages/*/src/*.d.ts
+rm -f packages/*/src/**/*.js
+rm -f packages/*/src/**/*.d.ts
